9d84a013ba8a01c65d4f565e6d6a4d11871a45e0
Carmen Birkle <birkle@carmenbirkle.de>
2023-03-18 09:17:45 +0100

    render new

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ const limit = 20;
 
 window.onscroll = function() {stickyNavbar()};
 
+async function loadPokemon(){
+  let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+  response = await fetch(url);
+  let responseToJson = await response.json();
+  console.log('aktuelles response', responseToJson); // kann raus wenns funzt
+  renderPokemoninfo(responseToJson); 
+  offset += limit; // Erhöhe offset um limit, um die nächsten 20 Pokémon beim nächsten Laden zu erhalten
+}
+
+// async function loadPokemon(){
+//   let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+//   response = await fetch(url);
+//   let responseToJson = await response.json();
+//   console.log('aktuelles response', responseToJson); // kann raus wenns funzt
+//   renderPokemoninfo(responseToJson); 
+// }
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+      loadPokemon();
+  }
+});
+
 
 function stickyNavbar() {
   if (window.pageYOffset >= sticky) {
@@ -22,14 +46,7 @@ function stickyNavbar() {
 
 
 
-async function loadPokemon(){
-   
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-    response = await fetch(url);
-    let responseToJson = await response.json();
-    console.log('aktuelles response', responseToJson); // kann raus wenns funzt
-    renderPokemoninfo(responseToJson); 
-}
+
 
 function renderPokemoninfo(responseToJson){
     let results = responseToJson['results']
@@ -135,16 +152,16 @@ function getTypeAllocation(id, typeValue) {
 
 
 
-window.addEventListener('scroll', async () => {
-  const scrollPosition = window.innerHeight + window.scrollY;
-  const bodyHeight = document.body.offsetHeight;
+// window.addEventListener('scroll', async () => {
+//   const scrollPosition = window.innerHeight + window.scrollY;
+//   const bodyHeight = document.body.offsetHeight;
 
-  // Wenn das Ende des Dokuments erreicht wurde
-  if (scrollPosition >= bodyHeight) {
-    // Erhöhen Sie den Offset-Wert, um weitere Pokémon zu laden
-    await appendPokemonInfo();
-  }
-});
+//   // Wenn das Ende des Dokuments erreicht wurde
+//   if (scrollPosition >= bodyHeight) {
+//     // Erhöhen Sie den Offset-Wert, um weitere Pokémon zu laden
+//     await appendPokemonInfo();
+//   }
+// });
 
 
 
@@ -159,67 +176,6 @@ function closeByButton(){
   document.getElementById('main-content').classList.remove("d-none");
   document.getElementById('header').classList.remove("d-none");
 }
-
-
-
-
-
-
-
-        // const ctx = document.getElementById('myChart');
-        // const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
-        // new Chart(ctx, 
-        //     {
-        //   type: 'bar',
-        //   data: {
-        //     labels:labels,
-        //     datasets: [{
-
-        //         axis: 'y',
-        //         label: 'Pokemon State',
-        //         borderRadius: 5,
-        //         data: [65, 59, 80, 81, 56, 10],
-        //         fill: false,
-        //         backgroundColor: [
-        //         'rgba(255, 99, 132, 0.2)',
-        //         'rgba(255, 159, 64, 0.2)',
-        //         'rgba(255, 205, 86, 0.2)',
-        //         'rgba(75, 192, 192, 0.2)',
-        //         'rgba(54, 162, 235, 0.2)',
-        //         'rgba(153, 102, 255, 0.2)',
-        //         'rgba(201, 203, 207, 0.2)'
-        //         ],
-        //         borderColor: [
-        //         'rgb(255, 99, 132)',
-        //         'rgb(255, 159, 64)',
-        //         'rgb(255, 205, 86)',
-        //         'rgb(75, 192, 192)',
-        //         'rgb(54, 162, 235)',
-        //         'rgb(153, 102, 255)',
-        //         'rgb(201, 203, 207)'
-        //         ],
-        //         borderWidth: 1
-
-              
-        //     }]
-        //   },
-        //   options: {
-        //     responsive: true,
-        //     indexAxis: 'y',
-        //     plugins: {
-        //         legend: {
-        //           display: false
-        //         }
-        //       },
-        //     scales: {
-        //       y: {
-        //         beginAtZero: true
-        //       }
-        //     }
-        //   }
-        // }
-        
-        // );
 
 
  
